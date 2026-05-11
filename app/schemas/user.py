e0da26ec0bd27978d3904d486175dc55fe707622
IlyaKutepov74 <Ilaykutepov@gmail.com>
2026-05-11 10:09:37 +0500

    Update user.py

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,19 +1,22 @@
-from pydantic import BaseModel, ConfigDict
-
+from pydantic import BaseModel, ConfigDict, EmailStr
+from datetime import datetime
+from typing import Optional
 
 class UserBase(BaseModel):
-    email: str
-    first_name: str | None = None
-    last_name: str | None = None
-
+    username: str
+    email: EmailStr  
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
 
 class UserCreate(UserBase):
     password: str
-
 
 class UserRead(UserBase):
     id: int
     is_active: bool
     is_superuser: bool
+    role: str  
+    created_at: datetime
+    last_login: Optional[datetime] = None
 
     model_config = ConfigDict(from_attributes=True)
