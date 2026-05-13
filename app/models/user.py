4785faed94617854a04e304446d33ac790ec2e1e
Adelina Ilyasova <i-ilyasovAdelina@ya.ru>
2026-05-14 00:14:11 +0700

    rename tablename in user.py

--- app/models/user.py
+++ app/models/user.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from app.core.db import Base
 
 class User(Base):
-    __tablename__ = "library_project"  
+    __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     username: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
