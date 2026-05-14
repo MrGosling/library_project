e7aa708dd3dbbedce8d947cc93862a92b29bef7e
IlyaKutepov74 <Ilaykutepov@gmail.com>
2026-05-14 18:29:36 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 
 from app.api.routers import main_router
 from app.core.config import settings
+from app.api.endpoints import user
 
 app = FastAPI(title=settings.title)
 
 app.include_router(main_router)
+app.include_router(user.router, prefix="/api/v1")
